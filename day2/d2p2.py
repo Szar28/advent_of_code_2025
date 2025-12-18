d770d6582f0bbe9a62ae382149a63794a8e8389c
Matthew S <100392783+Szar28@users.noreply.github.com>
2025-12-17 21:05:47 -0500

    completed day 2

--- day2/d2p2.py
+++ day2/d2p2.py
@@ -1,3 +1,13 @@
+"""
+same rules but if pattern repeats twice
+ex: 212121, 123123123
+invalid IDs
+"""
+
+def duplicate(s: str):
+    i = (s+s).find(s, 1, -1)
+    return None if i == -1 else s[:i]
+
 total = 0
 with open("input2.txt") as f:
     for line in f:
@@ -8,11 +18,10 @@ with open("input2.txt") as f:
             lower_end, upper_end = map(int, sequence.split('-'))
             for x in range(lower_end, upper_end+1):
                 front = str(x)
+                tester = duplicate(front)
                 front = front[:len(front)//2]
                 back = str(x)
-                back = back[len(back)//2:]
-
-                if front == back:
+                back = back[len(back)//2:] 
+                if front == back or tester:
                     total += x
-
 print(total)
